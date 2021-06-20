9a5927276de82d2e038e09843ede822032beff60
Jeffrey Chu <71513654+jschu713@users.noreply.github.com>
2021-06-20 14:36:07 -0500

    redacted username and password

--- database/dbcon.js
+++ database/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs290_chujef',
-  password: '2203',
-  database: 'cs290_chujef'
+  user: '[username]',
+  password: '[username]',
+  database: '[username]'
 });
 
 module.exports.pool = pool;
